32a997aaecb1b559dacc0337bdda189de6aef10a
Jorge <jogonzal@microsoft.com>
2016-11-24 15:57:42 -0800

    Finish first problem

--- p1/matrixmultiply.cpp
+++ p1/matrixmultiply.cpp
@@ -8,56 +8,49 @@
 #include <sys/time.h>
 #include <mpi.h>
 
-void printRow(int columns, int *arr){
-    printf("[");
-    for(int c = 0; c < columns; c++)
-    {
-        int element = arr[c];
-        printf("%d\t", element);
-    }
-    printf("]\n");
+void host_muls(int *matrix,
+	int *vector,
+	int *result,
+	int dim) {
+	for (int r = 0; r < dim; r++) {
+		int acc = 0;
+		for (int c = 0; c < dim; c++) {
+			int matrixNumber = matrix[r * dim + c];
+			int vectorNumber = vector[c];
+			acc += matrixNumber * vectorNumber;
+		}
+		result[r] = acc;
+	}
 }
 
-void printInputParameters(int inputMatrixRows, int inputMatrixColumns, int inputVectorRows, int **matrix, int *vector){
-    printf("\n The matrix is %d x %d\n", inputMatrixRows, inputMatrixColumns);
-    for(int r = 0; r < inputMatrixRows; r++){
-        printRow(inputMatrixColumns, matrix[r]);
-    }
-    printf("\nThe vector is %d x 1\n", inputVectorRows);
-    printRow(inputVectorRows, vector);
+void printVector(int *vector, int dim) {
+	printf("[");
+	for (int i = 0; i < dim; i++) {
+		printf("%d\t", vector[i]);
+	}
+	printf("]\n");
 }
 
-void singleThreadedMatrixMultiply(int inputMatrixRows, int inputMatrixColumns, int **matrix, int *vector, int *result){
-    for(int r = 0; r < inputMatrixRows; r++){
-        int *rowToMultiply = matrix[r];
-        int acc = 0;
-        for(int c = 0; c < inputMatrixColumns; c++){
-            int localResult = rowToMultiply[c] * vector[c];
-            acc = acc + localResult;
-        }
-        result[r] = acc;
-    }
-}
+void writeArrayToFile(int columns, int *arr) {
+	FILE *f = fopen("output.txt", "w+");
+	if (f == NULL)
+	{
+		printf("Error opening file!\n");
+		exit(1);
+	}
 
-void writeArrayToFile(int columns, int *arr){
-    FILE *f = fopen("output.txt", "w+");
-    if (f == NULL)
-    {
-        printf("Error opening file!\n");
-        exit(1);
-    }
-    
-    for(int c = 0; c < columns; c++)
-    {
-        int element = arr[c];
-        if (c == columns - 1){
-            fprintf(f, "%d", element);
-        } else {
-            fprintf(f, "%d,", element);
-        }
-    }
-    
-    fclose(f);
+	for (int c = 0; c < columns; c++)
+	{
+		int element = arr[c];
+		if (c == columns - 1) {
+			fprintf(f, "%d", element);
+		}
+		else {
+			fprintf(f, "%d,", element);
+		}
+	}
+
+	fclose(f);
 }
 
 int main(int argc, char *argv[]) {
@@ -68,11 +61,14 @@ int main(int argc, char *argv[]) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	printf("SIZE = %d RANK = %d\n",size,rank);
 
+	int   *matrix, *vector, *resultHost, *resultMpi;
+	int matrixSize;
+	int i;
     if (argc != 2) {
         printf("{%d}: Usage: %s inputfile\n", rank, argv[0]);
         return -1;
     }
-	
+	// Only the first processor will do the file reading into arrays
 	if (rank == 0){
 		char* inputFile = argv[1];
 		printf("{%d}: Will do processing on inputfile %s.\n", rank, inputFile);
@@ -85,62 +81,72 @@ int main(int argc, char *argv[]) {
 		bool arraysAreInitialized = false;
 		bool matrixFilled = false;
 		bool vectorFilled = false;
-		
+
 		int matrixOffset = 0;
 		int vectorOffset = 0;
 		int expectedMatrixElements = -1;
 		int expectedVectorElements = -1;
-		
-		int **matrix;
-		int *vector;
 
-		printf("{%d}: Starting loop...\n", rank);
 		while (fgets(line, sizeof(line), file)) {
 			char *element = strtok(line, ",");
-			while (element != NULL){
+			while (element != NULL) {
 				int elementAsInteger = atoi(element);
 
 				//printf("%d\n", elementAsInteger);
-				
+
 				// State machine goes here
-				if (inputMatrixRows == -1){
+				if (inputMatrixRows == -1) {
 					inputMatrixRows = elementAsInteger;
-				} else if (inputMatrixColumns == -1){
+				}
+				else if (inputMatrixColumns == -1) {
 					inputMatrixColumns = elementAsInteger;
-				} else {
-					if (!arraysAreInitialized){
+				}
+				else {
+					if (!arraysAreInitialized) {
+						if (inputMatrixRows != inputMatrixColumns) {
+							printf("Dimensions must match on matrix (square). %d and %d don't match.\n",
+								inputMatrixColumns, inputMatrixRows);
+							exit(-__LINE__);
+						}
 						printf("Dimensions are %d x %d. Creating arrays...\n", inputMatrixRows, inputMatrixColumns);
-						vector = (int *) malloc(sizeof(int) * inputMatrixColumns);
-						matrix = (int **) malloc(sizeof(int*) * inputMatrixRows);
-						for(int i = 0; i < inputMatrixRows; i++){
-							matrix[i] = (int *) malloc(sizeof(int) * inputMatrixColumns);
+						matrix = (int*)malloc(sizeof(*matrix) * inputMatrixRows * inputMatrixColumns);
+						vector = (int*)malloc(sizeof(*vector) * inputMatrixColumns);
+						resultHost = (int*)malloc(sizeof(*resultHost) * inputMatrixRows);
+						resultMpi = (int*)malloc(sizeof(*resultMpi) * inputMatrixRows);
+						for (i = 0; i < inputMatrixRows * inputMatrixColumns; i++) {
+							matrix[i] = 0;
+						}
+						for (i = 0; i < inputMatrixColumns; i++) {
+							vector[i] = 0;
 						}
 						expectedMatrixElements = inputMatrixRows * inputMatrixColumns;
 						expectedVectorElements = inputMatrixColumns;
 						arraysAreInitialized = true;
 					}
-					
-					
-					if (!matrixFilled){
-						int r = matrixOffset/inputMatrixColumns;
+
+
+					if (!matrixFilled) {
+						int r = matrixOffset / inputMatrixColumns;
 						int c = matrixOffset%inputMatrixColumns;
-						matrix[r][c] = elementAsInteger;
+						matrix[r * inputMatrixColumns + c] = elementAsInteger;
 						matrixOffset++;
-						if (matrixOffset == expectedMatrixElements){
+						if (matrixOffset == expectedMatrixElements) {
 							matrixFilled = true;
 						}
-					} else if (!vectorFilled){
+					}
+					else if (!vectorFilled) {
 						vector[vectorOffset] = elementAsInteger;
 						vectorOffset++;
-						if (vectorOffset == expectedVectorElements){
+						if (vectorOffset == expectedVectorElements) {
 							vectorFilled = true;
 						}
-					} else {
+					}
+					else {
 						printf("\nERROR IN INPUT!!!!!!!\nToo many numbers?");
 						return -1;
 					}
 				}
-				
+
 				// Advance to next
 				element = strtok(NULL, ",");
 			}
@@ -151,43 +157,69 @@ int main(int argc, char *argv[]) {
 			return -1;
 		}
 		
-		int inputVectorRows = inputMatrixColumns;
-		//printInputParameters(inputMatrixRows, inputMatrixColumns, inputVectorRows, matrix, vector);
-		fclose(file);
-
-		// At this point, we are done reading from the file. We will perform matrix multiply now
-
-		printf("Running BFS...\n\n");
+		matrixSize = inputMatrixColumns;
 		
-		// Matrix multiply - grab the entire vector and the ith row, then multiply and sum one by one
-		int *result = (int*) malloc(sizeof(int) * inputVectorRows);
-		
-		struct timeval start, end;
-		gettimeofday(&start, NULL);
-
-		singleThreadedMatrixMultiply(inputMatrixRows, inputMatrixColumns, matrix, vector, result);
-
-		gettimeofday(&end, NULL);
-
-		printf("Done!===============\n");
-		printf("Time taken: %ld\n", ((end.tv_sec * 1000000 + end.tv_usec)
-		  - (start.tv_sec * 1000000 + start.tv_usec)));
-		printf("\n\n");
-		
-
-		printf("The resulting vector is\n");
-		printRow(inputVectorRows, result);
-		
-		printf("\nI wrote this to output.txt.\n\n");
-		writeArrayToFile(inputMatrixColumns, result);
-		
-		free(vector);
-		free(result);
-		for(int i = 0; i < inputMatrixRows; i++){
-			free(matrix[i]);
-		}
-		free(matrix);
+		//DoMPIMultiplication(inputVectorRows, matrix, vector);
 	}
+	
+	MPI_Bcast(&matrixSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	printf("{%d}: Matrix size: %d inputfile\n", rank, matrixSize);
+	if (rank != 0){
+		// Do allocations
+		matrix = (int*)malloc(sizeof(*matrix) * matrixSize * matrixSize);
+		vector = (int*)malloc(sizeof(*vector) * matrixSize);
+		resultHost = (int*)malloc(sizeof(*resultHost) * matrixSize);
+		resultMpi = (int*)malloc(sizeof(*resultMpi) * matrixSize);
+	}
+	
+	MPI_Bcast(matrix, matrixSize * matrixSize, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(vector, matrixSize, MPI_INT, 0, MPI_COMM_WORLD);
+	
+	// Now that everybody has the matrix and vector, we should do matrix multiplication
+	int chunkSize = matrixSize / size;
+	{
+		int localChunk = chunkSize;
+		if (rank == size - 1){
+			localChunk = matrixSize - chunkSize * (size - 1);
+		}
+		for(int row = 0; row < localChunk; row++){
+			int offset = row + rank * chunkSize;
+			int acc = 0;
+			for(int col = 0; col < matrixSize; col++){
+				acc += vector[col] * matrix[offset * matrixSize + col];
+			}
+			resultMpi[offset] = acc;
+		}
+	}
+	
+	// Now that everybody calculated what they should, some sending needs to be done
+	if (rank == 0){
+		for(int i = 1; i < size; i++){
+			int localChunk = chunkSize;
+			if (i == size - 1){
+				localChunk = matrixSize - chunkSize * (size - 1);
+			}
+			int offset = i * chunkSize;
+			MPI_Recv(&resultMpi[offset], localChunk, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+	} else {
+		int localChunk = chunkSize;
+		if (rank == size - 1){
+			localChunk = matrixSize - chunkSize * (size - 1);
+		}
+		int offset = rank * chunkSize;
+		MPI_Send(&resultMpi[offset], localChunk, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
+	
+	if (rank == 0){
+		printVector(resultMpi, matrixSize);
+		writeArrayToFile(matrixSize, resultMpi);
+	}
+	
+	free(matrix);
+	free(vector);
+	free(resultHost);
+	free(resultMpi);
 	
 	MPI_Finalize();   
 	return(0);
